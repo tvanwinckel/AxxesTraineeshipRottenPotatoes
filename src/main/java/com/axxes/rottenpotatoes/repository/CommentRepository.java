1de5907b5fd670f2a0c747c043ed14a7da5a8c06
Thomas Van Winckel <vanwinckelthomas@gmail.com>
2018-08-11 16:45:18 +0200

    Added a comment repository.

--- src/main/java/com/axxes/rottenpotatoes/repository/CommentRepository.java
+++ src/main/java/com/axxes/rottenpotatoes/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.axxes.rottenpotatoes.repository;
 
-public interface CommentRepository {
+import com.axxes.rottenpotatoes.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- src/main/java/com/axxes/rottenpotatoes/repository/MovieRepository.java
+++ src/main/java/com/axxes/rottenpotatoes/repository/MovieRepository.java
@@ -2,6 +2,8 @@ package com.axxes.rottenpotatoes.repository;
 
 import com.axxes.rottenpotatoes.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
